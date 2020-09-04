f31f42876dcf1f77a55e9ed538d510c00f464973
clb-alper <52celebialper@gmail.com>
2020-09-04 14:39:39 +0300

    director schema

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name : String,
-    surname : String,
-    bio : String,
+    name : {
+        type : String,
+        maxlength : 40,
+        minlength : 2,
+
+    },
+    surname : {
+        type:  String,
+        maxlength : 40,
+        minlength : 2
+    },
+    bio : {
+        type : String,
+        maxlength : 4000,
+        minlength : 100,
+    },
     createdAt: {
         type : Date,
         default : Date.now
