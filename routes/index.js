9280c57423d59ddedd5e33b5b7cce40c548f88a6
clb-alper <52celebialper@gmail.com>
2020-09-06 02:41:10 +0300

    jsonwebtoken /authenticate

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,52 @@ const router = express.Router();
 
 //bcrypt
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken')
 //model
 const User = require('../models/User');
 
 /* GET home page. */
 router.get('/', (req, res, next) =>{
   res.render('index', { title: 'Express' });
+});
+
+
+router.post('/authenticate', (req,res)=>{
+  const {username , password} = req.body;
+
+  User.findOne({
+    username
+  }, (err, user)=>{
+    if(err){
+      throw err;
+    }
+    if(!user){
+      res.json({
+        status:false,
+        message : 'Authentication failed, user not found.'
+      })
+    }else{
+      bcrypt.compare(password, user.password).then((result)=>{
+        if(!result){
+          res.json({
+            status: false,
+            message : 'Authentication failed, wrong password'
+          });
+        }else{
+          const payload = {
+            username
+          };
+          const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+            expiresIn : 720 //12 saat
+          });
+          res.json({
+            status: true,
+            token : token
+          })
+        }
+      });
+    }
+  });
 });
 
 
